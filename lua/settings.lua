4f7dacb22cc1309efa91a193ef06207719ec304d
yakumo22 <2358044108@qq.com>
2024-07-24 21:41:07 +0800

    chaneg cwd on soft-link-dir

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,15 +1,30 @@
 -- basics
 -- make zsh files recognized as sh for bash-ls & treesitter
-vim.filetype.add {
-	extension = {
-		zsh = "sh",
-		sh = "sh", -- force sh-files with zsh-shebang to still get sh as filetype
-	},
-	filename = {
-		[".zshrc"] = "sh",
-		[".zshenv"] = "sh",
-	},
-}
+vim.filetype.add({
+    extension = {
+        zsh = "sh",
+        sh = "sh", -- force sh-files with zsh-shebang to still get sh as filetype
+    },
+    filename = {
+        [".zshrc"] = "sh",
+        [".zshenv"] = "sh",
+    },
+})
+
+-- surpport soft link
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        local path = vim.fn.expand("%:p")
+        local stat = vim.loop.fs_stat(path)
+
+        if vim.loop.fs_stat(path) then
+            vim.api.nvim_buf_set_name(0, vim.loop.fs_realpath(path))
+            vim.api.nvim_command("edit")
+        end
+
+        vim.fn.chdir(vim.loop.fs_realpath(vim.fn.getcwd()))
+    end,
+})
 
 -- highlight after copy
 vim.api.nvim_create_autocmd({ "textyankpost" }, {
